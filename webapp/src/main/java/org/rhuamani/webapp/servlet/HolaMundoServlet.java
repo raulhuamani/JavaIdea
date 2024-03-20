e4d69cb6718645b554b3b01173d9073fb5fdc87e
raulhuamani <raul.huamani.h@gmail.com>
2024-03-20 00:13:50 -0500

    S40 - Jakarta - Servlet, enviar parametros GET de la url o Query String

---
+++ webapp/src/main/java/org/rhuamani/webapp/servlet/HolaMundoServlet.java
@@ -0,0 +1,32 @@
+package org.rhuamani.webapp.servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/hola-mundo")
+public class HolaMundoServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException{
+        resp.setContentType("text/html");
+        PrintWriter out = resp.getWriter();
+
+        out.println("<!DOCTYPE html>");
+        out.println("<html>");
+        out.println("    <head>");
+        out.println("        <meta charset=\"UTF-8\">");
+        out.println("        <title>Hola Mundo Servlet</title>");
+        out.println("    </head>");
+        out.println("    <body>");
+        out.println("        <h1>Hola Mundo Servlet!!</h1>");
+        out.println("    </body>");
+        out.println("</html>");
+        out.close();
+    }
+}
