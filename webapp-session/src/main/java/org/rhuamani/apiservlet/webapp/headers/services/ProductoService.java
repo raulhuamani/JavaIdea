7fd4c988bbc894dfd9d93be4dad35ac541c83127
raulhuamani <raul.huamani.h@gmail.com>
2024-03-22 23:47:45 -0500

    S44 - Jakarta - Httpsession - guardar el inicio en la session

---
+++ webapp-session/src/main/java/org/rhuamani/apiservlet/webapp/headers/services/ProductoService.java
@@ -0,0 +1,11 @@
+package org.rhuamani.apiservlet.webapp.headers.services;
+
+import org.rhuamani.apiservlet.webapp.headers.models.Producto;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ProductoService {
+    List<Producto> listar();
+    Optional<Producto> buscarProducto(String nombre);
+}
